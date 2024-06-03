a134fdac3fb686355779cefe0e2044df1f20ae00
ellie <hkj2855@gmail.com>
2024-06-03 16:35:27 +0900

    테스트 설치 첫번째

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Swiper as SwiperType} from 'swiper/types'; // Swiper 타입 임포트
+
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/autoplay';
+
 import './App.scss';
 
-function App() {
+
+
+
+function App () {
   return (
     <div className="mainWrap">
-    
+      {/* 첫 번째 Swiper */}
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper: SwiperType) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
     </div>
   );
 }

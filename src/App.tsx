c21238bb88ac35c4c30504d232d1f13cf1f43aa1
ellie <hkj2855@gmail.com>
2024-06-03 16:42:44 +0900

    테스트 마크업 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Swiper as SwiperType} from 'swiper/types'; // Swiper 타입 임포트
+import {EffectFade, Navigation, Pagination} from 'swiper/modules';
 
 
 import 'swiper/scss';
@@ -18,6 +19,7 @@ function App () {
     <div className="mainWrap">
       {/* 첫 번째 Swiper */}
       <Swiper
+        className="normalSwiper"
         spaceBetween={50}
         slidesPerView={3}
         onSlideChange={() => console.log('slide change')}
@@ -27,6 +29,26 @@ function App () {
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+      
+      {/* 두 번째 Swiper */}
+      <Swiper
+        className="fadeSwiper"
+        effect={'fade'}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[ EffectFade, Navigation, Pagination ]}
+        spaceBetween={30}
+        slidesPerView={2}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper: SwiperType) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide A</SwiperSlide>
+        <SwiperSlide>Slide B</SwiperSlide>
+        <SwiperSlide>Slide C</SwiperSlide>
+        <SwiperSlide>Slide D</SwiperSlide>
       </Swiper>
     </div>
   );

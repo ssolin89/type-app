e864f05d7961fb78afe03e9849e5896d274a2d0b
ellie <hkj2855@gmail.com>
2024-06-05 16:50:31 +0900

    테스트 마크업 추가 변경중

--- src/slider/SwiperComponentThumb.tsx
+++ src/slider/SwiperComponentThumb.tsx
@@ -19,45 +19,7 @@ const SwiperComponentThumbs: React.FC = () => {
   const [ thumbsSwiper, setThumbsSwiper ] = useState<SwiperType | null>(null);
   
   return (
-    <>
-      <Swiper
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null}}
-        modules={[ FreeMode, Navigation, Thumbs ]}
-        className="mySwiper2"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="nature-1"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="nature-2"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="nature-3"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="nature-4"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="nature-5"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="nature-6"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="nature-7"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="nature-8"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="nature-9"/>
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-10.jpg" alt="nature-10"/>
-        </SwiperSlide>
-      </Swiper>
+    <div className="swiperThumbsArea">
       <Swiper
         onSwiper={setThumbsSwiper}
         spaceBetween={10}
@@ -91,14 +53,41 @@ const SwiperComponentThumbs: React.FC = () => {
         <SwiperSlide>
           <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="nature-8"/>
         </SwiperSlide>
+      </Swiper>
+      <Swiper
+        spaceBetween={10}
+        navigation={true}
+        thumbs={{swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null}}
+        modules={[ FreeMode, Navigation, Thumbs ]}
+        className="mySwiper2"
+      >
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="nature-9"/>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="nature-1"/>
         </SwiperSlide>
         <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-10.jpg" alt="nature-10"/>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="nature-2"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="nature-3"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="nature-4"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="nature-5"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="nature-6"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="nature-7"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="nature-8"/>
         </SwiperSlide>
       </Swiper>
-    </>
+    
+    </div>
   );
 };
 
